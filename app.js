cdcade6159cf1bf9bb0d58868b2088491ebfef17
boy <boy.bouwense@gmail.com>
2019-05-27 21:30:42 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const port = 3000;
+
+app.get('/humidity/:epoch', (req, res) => {
+
+    let epoch = parseInt(req.params["epoch"]);
+    let endEpoch = epoch + 86400;
+
+    let con = mysql.createConnection({
+        host: "localhost",
+        user: "isen",
+        password: "isen123",
+        database: "isen"
+    });
+
+    con.connect((err) =>{
+        if (err) throw err;
+        con.query(`SELECT * FROM Humidity where timestamp >= ${epoch} and timestamp <= ${endEpoch}`, (err,result,fields) =>{
+            res.send(result)
+        })
+    })
+});
+
+app.get('/light/:epoch', (req, res) => {
+
+    let epoch = parseInt(req.params["epoch"]);
+    let endEpoch = epoch + 86400;
+
+    console.log(epoch);
+
+
+    let con = mysql.createConnection({
+        host: "localhost",
+        user: "isen",
+        password: "isen123",
+        database: "isen"
+    });
+
+    con.connect((err) =>{
+        if (err) throw err;
+        con.query(`SELECT * FROM Light where timestamp >= ${epoch} and timestamp <= ${endEpoch}`, (err,result,fields) =>{
+            res.send(result)
+        })
+    })
+});
+
+app.get('/temperature/:epoch', (req, res) =>{
+
+    let epoch = parseInt(req.params["epoch"]);
+    let endEpoch = epoch + 86400;
+
+    let con = mysql.createConnection({
+        host: "localhost",
+        user: "isen",
+        password: "isen123",
+        database: "isen"
+    });
+
+    con.connect((err) =>{
+        if (err) throw err;
+        con.query(`SELECT * FROM Temperature where timestamp >= ${epoch} and timestamp <= ${endEpoch}`, (err,result,fields) =>{
+            res.send(result)
+        })
+    })
+});
+
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
+module.exports = app;
