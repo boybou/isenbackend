bfe0c81df83c0a91dd05e68a3df99f92be96c363
boy <boy.bouwense@gmail.com>
2019-05-29 01:55:06 +0200

    backend update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const port = 3000;
+const fs = require("fs");
 
 app.get('/humidity/:epoch', (req, res) => {
 
@@ -61,6 +62,27 @@ app.get('/temperature/:epoch', (req, res) =>{
     con.connect((err) =>{
         if (err) throw err;
         con.query(`SELECT * FROM Temperature where timestamp >= ${epoch} and timestamp <= ${endEpoch}`, (err,result,fields) =>{
+            res.send(result)
+        })
+    })
+});
+
+app.get('/sound/:epoch', (req, res) =>{
+
+    let epoch = parseInt(req.params["epoch"]);
+    let endEpoch = epoch + 86400;
+
+    let con = mysql.createConnection({
+        host: "localhost",
+        user: "isen",
+        password: "isen123",
+        database: "isen"
+    });
+
+    con.connect((err) =>{
+        if (err) throw err;
+        con.query(`SELECT * FROM Sound where timestamp >= ${epoch} and timestamp <= ${endEpoch}`, (err,result,fields) =>{
+            console.log(result);
             res.send(result)
         })
     })
